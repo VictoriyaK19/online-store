2ebbb774be2f62959585b0596518dda4f6b2a2e0
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-16 15:55:18 +0200

    creating router path

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -3,9 +3,3 @@
 </template>
 
 
-<script>
-
-export default {
-
-}
-</script>
--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TheHome from "./pages/TheHome.vue";
 import NotFound from "./pages/NotFound.vue";
+import TheStore from "@/pages/TheStore";
+import FAQuestions from '@/pages/FAQuestions';
+import AboutUs from '@/pages/AboutUs';
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: "/", redirect: "/home" },
-    { path: "/home", component: TheHome },
-
-    { path: "/:notFound(.*)", component: NotFound },
-  ],
-});
+    history: createWebHistory(),
+    routes: [
+      { path: "/", redirect: "/home" },
+      { path: "/home", component: TheHome },
+      { path: "/store", component: TheStore },
+      { path: "/abutus", component: AboutUs },
+      { path: "/faq", component: FAQuestions },
+  
+      { path: "/:notFound(.*)", component: NotFound },
+    ],
+  });
 
 export default router;
