069102d9334e55d10a5e0aea748cbbad218b3b85
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-19 12:01:48 +0200

    fixed responsive design

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -4,7 +4,7 @@
       
       <div :class="!isResponsive ? 'topnav' : 'topnav responsive'">
         <router-link to="/" class="left">Brand name</router-link>
-        <router-link to="/store" class="center">Store</router-link>
+        <router-link to="/store" class="center1">Store</router-link>
         <router-link to="/aboutus" class="center">About us</router-link>
         <router-link to="/faq" class="center2">FAQ</router-link>
         <router-link to="/mycart" class="right">
@@ -39,31 +39,37 @@ h1 {
   margin: 0;
   color: white;
 }
+
 h2,
 p {
   color: white;
 }
+
 body {
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
 }
+
 header nav {
   padding-top: 1rem;
   width: 90%;
   margin: auto;
-}
-.topnav {
-  overflow: hidden;
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
+.topnav {
+  overflow: hidden;
+}
+
 .topnav a {
+  margin: 0;
   text-decoration: none;
   color: #ffffff;
   display: inline-block;
-
+  padding: 1rem 1.5rem;
+  border: 1px solid transparent;
 }
 
 .topnav a:hover {
@@ -82,49 +88,64 @@ header nav {
   padding: 1rem;
 }
 
-.left {
+.topnav .left {
   margin-right: auto;
   font-size: 25px;
   font-weight: bold;
 }
+.topnav .center1 {
+  margin: 0 0vw 0 17vw;
 
-.center {
-  margin: 0 3rem;
 }
-.center2 {
-  margin: 0 29vw 0 3rem ;
-}
-.right {
-  margin-left: 2rem;
+.topnav .center {
+  margin: 0 3vw;
 }
 
+.topnav .center2 {
+  margin: 0 20vw 0 0;
+}
+
+.topnav .right {
+  margin-left: 2vw;
+}
 
 @media screen and (max-width: 667px) {
   .topnav a:not(:first-child) {
     display: none;
   }
+
   .topnav .bars {
-    float: right;
     display: block;
     text-decoration: none;
     color: #ffffff;
     padding: 1.5rem 1.5rem;
-  }
-}
-
-@media screen and (max-width: 667px) {
-  .topnav.responsive {
-    position: relative;
-  }
-  .topnav.responsive .bars {
     position: absolute;
-    right: 0;
-    top: 0;
+    right: 20px;
+    top: 20px;
   }
+
+  .topnav.responsive .bars {
+    display: block;
+    text-decoration: none;
+    color: #ffffff;
+    padding: 1.5rem 1.5rem;
+    position: absolute;
+    right: 20px;
+    top: 20px;
+  }
+
   .topnav.responsive a {
     float: none;
     display: block;
     text-align: left;
   }
+  .topnav .center1,
+  .topnav .center,
+  .topnav .center2,
+  .topnav .right   {
+  margin: 0;
 }
+}
+
+
 </style>
