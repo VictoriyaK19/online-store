58b59a36cc19e96e931c9b8b4ff863dd8ba40462
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-01-29 09:24:23 +0200

    sample store

--- src/pages/TheStore.vue
+++ src/pages/TheStore.vue
@@ -1,20 +1,41 @@
 <template>
-    <div class="background">
-    <div>
-        <img src="" alt="">
+  <div class="background">
+    <div class="store">
+      <div class="product" v-for="(product, index) in products" :key="index">
+        <img src="required(@/assets/${product.image})" :alt="product.name" />
         <p>{{ products.productName }}</p>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
+import { products } from "@/store/store";
+
 export default {
-    data() {
-        return {
-            products:{
-                productName: 'Laptop'
-            }
-        }
-    }
-}
+  data() {
+    return {
+      products: products,
+    };
+  },
+};
 </script>
+
+
+<style>
+.store {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.product {
+  flex: 1 1 200px; /* Flexbox shorthand */
+  margin: 10px;
+  text-align: center;
+}
+
+.product img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,5 @@
+export const products = [
+    { name: 'Product 1', image: 'product1.jpg' },
+    { name: 'Product 2', image: 'product2.jpg' },
+    { name: 'Product 3', image: 'product3.jpg' },
+  ];
