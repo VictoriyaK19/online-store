c37b85fde9f174c585a3023c22333f25fb762828
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-02-01 16:05:16 +0200

    fixing faq style

--- src/pages/FAQuestions.vue
+++ src/pages/FAQuestions.vue
@@ -3,7 +3,10 @@
     <h1>Frequently Asked Questions</h1>
 
     <div v-for="(item, index) in faqItems" :key="index" class="faq-item">
-      <button @click="toggleAnswer(index)" class="question">{{ item.question }}</button>
+      <button @click="toggleAnswer(index)" class="question">
+        {{ item.question }}
+        <span class="favicon"><font-awesome-icon :icon="['fas', 'sort-down']" /></span>
+      </button>
       <p v-show="item.showAnswer" class="answer">{{ item.answer }}</p>
     </div>
   </div>
@@ -16,67 +19,78 @@ export default {
       faqItems: [
         {
           question: "What makes [Your Online Store Name] unique?",
-          answer: "At [Your Online Store Name], we pride ourselves on offering a curated selection of cutting-edge tech products coupled with unparalleled customer service.",
-          showAnswer: true
+          answer:
+            "At [Your Online Store Name], we pride ourselves on offering a curated selection of cutting-edge tech products coupled with unparalleled customer service.",
+          showAnswer: false,
         },
         {
           question: "How can I contact customer support?",
-          answer: "You can reach our customer support team via email at your@email.com or by phone at +123456789.",
-          showAnswer: false
+          answer:
+            "You can reach our customer support team via email at your@email.com or by phone at +123456789.",
+          showAnswer: false,
         },
         {
           question: "What payment methods do you accept?",
-          answer: "We accept all major credit cards, including Visa, Mastercard, American Express, and Discover. Additionally, we offer the convenience of PayPal for secure transactions.",
-          showAnswer: false
+          answer:
+            "We accept all major credit cards, including Visa, Mastercard, American Express, and Discover. Additionally, we offer the convenience of PayPal for secure transactions.",
+          showAnswer: false,
         },
         {
           question: "Do you offer international shipping?",
-          answer: "Yes, we do! We ship to select international destinations. Shipping costs and delivery times may vary depending on your location. Please note that additional customs fees and duties may apply.",
-          showAnswer: false
+          answer:
+            "Yes, we do! We ship to select international destinations. Shipping costs and delivery times may vary depending on your location. Please note that additional customs fees and duties may apply.",
+          showAnswer: false,
         },
         {
           question: "What is your return policy?",
-          answer: "We want you to be completely satisfied with your purchase. If for any reason you're not happy with your order, you can return it within [number of days] days for a full refund or exchange. Please refer to our Returns & Exchanges page for more information on the process.",
-          showAnswer: false
+          answer:
+            "We want you to be completely satisfied with your purchase. If for any reason you're not happy with your order, you can return it within [number of days] days for a full refund or exchange. Please refer to our Returns & Exchanges page for more information on the process.",
+          showAnswer: false,
         },
         {
           question: "Are your products covered by a warranty?",
-          answer: "Absolutely! Most of our products come with a manufacturer's warranty to ensure your peace of mind. The duration and terms of the warranty may vary depending on the product. Please refer to the product description or contact our customer support team for more information.",
-          showAnswer: false
+          answer:
+            "Absolutely! Most of our products come with a manufacturer's warranty to ensure your peace of mind. The duration and terms of the warranty may vary depending on the product. Please refer to the product description or contact our customer support team for more information.",
+          showAnswer: false,
         },
         {
           question: "How do I track my order?",
-          answer: "Once your order has been processed and shipped, you will receive a tracking number via email. You can use this tracking number to monitor the status and whereabouts of your package. If you encounter any issues with tracking your order, feel free to reach out to our customer support team for assistance.",
-          showAnswer: false
+          answer:
+            "Once your order has been processed and shipped, you will receive a tracking number via email. You can use this tracking number to monitor the status and whereabouts of your package. If you encounter any issues with tracking your order, feel free to reach out to our customer support team for assistance.",
+          showAnswer: false,
         },
         {
           question: "Can I cancel or modify my order after it's been placed?",
-          answer: "We understand that circumstances may change. If you need to cancel or modify your order, please contact us as soon as possible. We'll do our best to accommodate your request, but please note that orders are typically processed and shipped promptly to ensure timely delivery.",
-          showAnswer: false
+          answer:
+            "We understand that circumstances may change. If you need to cancel or modify your order, please contact us as soon as possible. We'll do our best to accommodate your request, but please note that orders are typically processed and shipped promptly to ensure timely delivery.",
+          showAnswer: false,
         },
         {
           question: "Do you offer bulk or corporate discounts?",
-          answer: "Yes, we do offer discounts for bulk orders and corporate purchases. Please reach out to our sales team at sales@email.com for more information on pricing and availability.",
-          showAnswer: false
+          answer:
+            "Yes, we do offer discounts for bulk orders and corporate purchases. Please reach out to our sales team at sales@email.com for more information on pricing and availability.",
+          showAnswer: false,
         },
         {
-          question: "How can I stay updated on new product releases and promotions?",
-          answer: "Stay in the loop by subscribing to our newsletter! Simply enter your email address in the signup form at the bottom of our website to receive exclusive updates on new arrivals, special offers, and more.",
-          showAnswer: false
+          question:
+            "How can I stay updated on new product releases and promotions?",
+          answer:
+            "Stay in the loop by subscribing to our newsletter! Simply enter your email address in the signup form at the bottom of our website to receive exclusive updates on new arrivals, special offers, and more.",
+          showAnswer: false,
         },
-      ]
+      ],
     };
   },
   methods: {
     toggleAnswer(index) {
       this.faqItems.forEach((item, i) => {
-      if (i !== index) {
-        item.showAnswer = false;
-      }
-    });
+        if (i !== index) {
+          item.showAnswer = false;
+        }
+      });
       this.faqItems[index].showAnswer = !this.faqItems[index].showAnswer;
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -89,8 +103,6 @@ export default {
   border-radius: 8px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 }
-
-
 h1 {
   margin-top: 0.5rem;
   margin-bottom: 2rem;
@@ -101,11 +113,15 @@ h1 {
 }
 
 .faq-item {
+  margin: 0 4rem ;
+  position: relative; 
   padding-top: 0.5rem;
   margin-bottom: 20px;
 }
 
 .question {
+  position: relative; /* Ensure positioning context for favicon */
+  padding-right: 30px; /* Adjust spacing for the favicon */
   cursor: pointer;
   font-size: 18px;
   font-weight: bold;
@@ -122,9 +138,24 @@ h1 {
   border-left: 2px solid #004aad;
 }
 
+ .favicon {
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-size: 18px;
+    color: #004aad;
+  } 
+
 @media screen and (max-width: 600px) {
+  .faq-item {
+  margin: 0 2rem 25px 2rem ;
+}
   .faq-container {
     padding: 10px;
   }
+  .favicon {
+    
+
+  }  
 }
 </style>
