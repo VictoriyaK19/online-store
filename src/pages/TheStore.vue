3e0e546a0f5c1386936a3895d0eed6bb30da7feb
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-02-06 17:32:32 +0200

    working on the store

--- src/pages/TheStore.vue
+++ src/pages/TheStore.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="background">
     <div class="store">
-      <div class="product" v-for="(product, index) in products" :key="index">
-        <img :src="require(`@/assets/products/${product.image}`)" :alt="product.name" />
-        <p>{{ product.name }}</p>
+      <div class="product" v-for="product in products" :key="product.id">
+        <img
+          :src="require(`@/assets/products/${product.image}`)"
+          :alt="product.name"
+        />
+        <h3 class="product-name">{{ product.name }}</h3>
+        <p class="product-price">{{ product.price }}</p>
+        <button>View Details</button>
+        <button @click="addToCart(product)">Add to cart</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       products: [
-    { name: 'Laptop', image: 'product1.jpg' },
-    { name: 'iPhone', image: 'product2.png' },
-    { name: 'Headphones', image: 'product3.jpg' },
-  ]
+        { id: "123", name: "Laptop", price: '1259,99$', image: "product1.jpg" },
+        { id: "124", name: "iPhone",  price: '1230,99$', image: "product2.png" },
+        { id: "125", name: "Headphones",  price: '123,99$', image: "product3.jpg" },
+      ],
     };
   },
 };
 </script>
-
 
 <style>
 .store {
@@ -35,30 +39,34 @@ export default {
 .product {
   margin: 1rem;
   padding: 1rem;
-  flex: 1 1 200px; /* Adjusted flex basis to maintain consistent width */
+  flex: 1 1 200px; 
   text-align: center;
-  max-width: 200px;
+  max-width: 230px;
   border-radius: 10px;
   border: 2px solid #004aad;
-  transition: transform 0.3s ease; 
+  transition: transform 0.3s ease;
   display: flex;
   flex-direction: column;
   justify-content: center;
 }
 
 .product:hover {
-  transform: scale(1.05); 
+  transform: scale(1.05);
 }
 
 .product img {
   max-width: 100%;
   height: auto;
-  border-radius: 10px 10px 0 0; 
+  border-radius: 10px 10px 0 0;
 }
-
+h3 {
+  margin: 0;
+  padding-bottom: 0.5rem;
+}
 p {
   margin: 0;
-  font-size: 16px; 
-}
+  font-size: 16px;
+  padding-bottom: 0.5rem;
 
+}
 </style>
