c02acaff2fb30952fe7275ebcf778c65421eec72
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-02-06 13:09:37 +0200

    adding footer icons

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,39 +1,39 @@
 <template>
-<footer class="footer">
-	<div class="footer__redes">
-		<ul class="footer__redes-wrapper">
-			<li>
-				<a href="#" class="footer__link">
-					<font-awesome-icon icon="facebook-f" />
-					facebook
-				</a>
-			</li>
-			<li>
-				<a href="#" class="footer__link">
-					
-					instagram
-				</a>
-			</li>
-			<li>
-				<a href="#" class="footer__link">
-					
-					youtube
-				</a>
-			</li>
-		</ul>
-	</div>
-	<div class="separador"></div>
-	<p class="footer__texto">Copyright @ 2024</p>
-</footer>
+  <footer class="footer">
+    <div class="footer__redes">
+      <ul class="footer__redes-wrapper">
+        <li>
+          <a href="#" class="footer-link"
+            ><i><font-awesome-icon :icon="['fab', 'square-facebook']" /></i>
+            facebook
+          </a>
+        </li>
+        <li>
+          <a href="#" class="footer-link"
+            ><i> <font-awesome-icon :icon="['fab', 'instagram']" /></i>
+            instagram
+          </a>
+        </li>
+        <li>
+          <a href="#" class="footer-link"
+            ><i> <font-awesome-icon :icon="['fab', 'youtube']" /></i>
+
+            youtube
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="separador"></div>
+    <p class="footer-text">Copyright © 2024</p>
+  </footer>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style scoped>
 .footer {
-  color: #fff; 
-  padding: 20px; 
+  color: #fff;
+  padding: 20px;
 }
 
 .footer__redes-wrapper {
@@ -44,41 +44,42 @@
 }
 
 .footer__redes-wrapper li {
-  margin: 0 10px; 
+  margin: 0 10px;
 }
 
-.footer__link {
+.footer-link {
   color: #fff;
   text-decoration: none;
   display: flex;
   align-items: center;
-  transition: color 0.3s; 
+  transition: color 0.3s;
+}
+i {
+  margin: auto 0.5rem;
 }
 
-.footer__link:hover {
-    text-shadow: 0 0 15px rgba(255, 255, 255, 0.9); 
+.footer-link:hover {
+  text-shadow: 0 0 15px rgba(255, 255, 255, 0.9);
 }
-
-
 
 .separador {
-  border-left: 1px solid #fff; 
-  margin: 0 20px; 
+  border-left: 1px solid #fff;
+  margin: 0 20px;
 }
 
-.footer__texto {
-  text-align: center; 
-  margin-top: 20px; 
+.footer-text {
+  text-align: center;
+  margin-top: 20px;
 }
-.footer__texto::before {
-  content: '';
+.footer-text::before {
+  content: "";
   display: block;
-  width: 40px; 
-  height: 2px; 
-  background-color: #fff; 
-  margin: 0 auto; 
+  width: 40px;
+  height: 2px;
+  background-color: #fff;
+  margin: 0 auto;
   margin-top: 2rem;
-  margin-bottom: 2rem; 
+  margin-bottom: 2rem;
   box-shadow: 0 2px 5px rgba(255, 255, 255, 5);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router.js';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import App from './App.vue'
 
 
 library.add(fas);
+library.add(fab);
 
 createApp(App)
 
