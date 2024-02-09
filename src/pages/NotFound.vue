b08e74c7aef92436cdcb6b8c6e36e0d44d5390f6
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-02-09 21:47:14 +0200

    working on not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,25 @@
 <template>
-    Not Found
+    <div>
+        <h1>404: Page Not Found</h1>
+    </div>
+
 </template>
+
+<style scoped>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+div {
+    height: 60vh;
+}
+
+h1 {
+    padding-top: 25vh;
+  font-size: 36px;
+  color: #333; 
+  text-align: center;
+}
+</style>
