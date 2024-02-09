044e9378b5b1f7b4a1e0228e2dc3e1ce99e25dd6
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-02-09 21:51:16 +0200

    not found product

--- src/pages/TheProduct.vue
+++ src/pages/TheProduct.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="img-wrap">
+    <div v-if="product">
+        <div class="img-wrap">
         <img :src="require(`@/assets/products/${product.image}`)" :alt="product.name">
     </div>
     <div class="product-details">
@@ -7,17 +8,25 @@
         <h3 class="price">{{ product.price }}</h3>
         <button class="add-to-cart">Add to cart</button>
     </div>
+    </div>
+    <div v-else>
+        <not-found></not-found>
+    </div>
 
 </template>
 
 <script>
 import { products } from "../temp-data";
+import NotFound from "./NotFound.vue";
 
 export default {
     data () {
         return {
             product: products.find(product => product.id === this.$route.params.productId)
         }
+    },
+    components: {
+        "not-found": NotFound
     }
 }
 </script>
