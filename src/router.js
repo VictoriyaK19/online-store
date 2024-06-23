c29beae41b2ac7ac68f260cd40b152fb9780af46
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-06-23 16:15:48 +0300

    fixed

--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TheHome from "./pages/TheHome.vue";
 
-import TheStore from "@/pages/TheStore";
-import FAQuestions from '@/pages/FAQuestions';
-import AboutUs from '@/pages/AboutUs';
+import TheStore from "@/pages/TheStore.vue";
+import FAQuestions from '@/pages/FAQuestions.vue';
+import AboutUs from '@/pages/AboutUs.vue';
 import MyProfile from "./pages/user/MyProfile.vue";
 import MyCart from "./pages/user/MyCart.vue";
 import NotFound from "./pages/NotFound.vue";
 import TheProduct from "./pages/TheProduct.vue";
-import TheAuth from '@/pages/TheAuth';
+import TheAuth from '@/pages/TheAuth.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(process.env.BASE_URL),
     routes: [
       { path: "/", redirect: "/home" },
       { path: "/home", component: TheHome },
