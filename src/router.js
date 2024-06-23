441c00fcbdf608b6edd9f9adeb6486a3890e021a
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-06-23 12:50:47 +0300

    fixed

--- src/router.js
+++ src/router.js
@@ -1,29 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
-import TheHome from "./pages/TheHome.vue";
 
-import TheStore from "@/pages/TheStore";
-import FAQuestions from '@/pages/FAQuestions';
-import AboutUs from '@/pages/AboutUs';
-import MyProfile from "./pages/user/MyProfile.vue";
-import MyCart from "./pages/user/MyCart.vue";
-import NotFound from "./pages/NotFound.vue";
-import TheProduct from "./pages/TheProduct.vue";
-import TheAuth from '@/pages/TheAuth';
+const TheHome = () => import("./pages/TheHome.vue");
+const TheStore = () => import("@/pages/TheStore.vue");
+const FAQuestions = () => import('@/pages/FAQuestions.vue');
+const AboutUs = () => import('@/pages/AboutUs.vue');
+const MyProfile = () => import("./pages/user/MyProfile.vue");
+const MyCart = () => import("./pages/user/MyCart.vue");
+const NotFound = () => import("./pages/NotFound.vue");
+const TheProduct = () => import("./pages/TheProduct.vue");
+const TheAuth = () => import('@/pages/TheAuth.vue');
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      { path: "/", redirect: "/home" },
-      { path: "/home", component: TheHome },
-      { path: "/store", component: TheStore },
-      { path: "/store/:productId", component: TheProduct },
-      { path: "/aboutus", component: AboutUs },
-      { path: "/faq", component: FAQuestions },
-      { path: "/myprofile", component: MyProfile },
-      { path: "/auth", component: TheAuth },
-      { path: "/mycart", component: MyCart },
-      { path: "/:notFound(.*)", component: NotFound },
-    ],
-  });
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/home" },
+    { path: "/home", component: TheHome },
+    { path: "/store", component: TheStore },
+    { path: "/store/:productId", component: TheProduct },
+    { path: "/aboutus", component: AboutUs },
+    { path: "/faq", component: FAQuestions },
+    { path: "/myprofile", component: MyProfile },
+    { path: "/auth", component: TheAuth },
+    { path: "/mycart", component: MyCart },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+});
 
 export default router;
+
