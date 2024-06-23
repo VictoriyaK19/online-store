53265b72bb6cf57ecc8b521cd94a751f3a933c7e
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-06-23 12:39:13 +0300

    fixed

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/online-store/",
+  publicPath: process.env.NODE_ENV === 'production' ? '/online-store/' : '/'
 })
 // module.exports = {
 //  publicPath: process.env.NODE_ENV === "production" ? "/online-store/" : "/",
