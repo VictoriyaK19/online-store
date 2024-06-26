24a2f33e0bc929d600a0a672a248d1f6c6b60fe1
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-06-26 17:39:23 +0300

    fixed bug data visualization

--- src/temp-data.js
+++ src/temp-data.js
@@ -2,11 +2,11 @@ export const products = [
   {
     id: "123",
     name: "Laptop",
-    price: "1259,99$",
+    price: "1259.99$",
     image: "product1.jpg",
   },
-  { id: "124", name: "iPhone", price: "1230,99$", image: "product2.png" },
-  { id: "125", name: "Headphones", price: "123,99$", image: "product3.jpg" },
-  { id: "126", name: "Samsung phone", price: "389,99$", image: "samsung.jpeg" },
-  { id: "127", name: "Wireless headphones", price: "389,99$", image: "wireless-headphones.jpg" },
+  { id: "124", name: "iPhone", price: "1230.99$", image: "product2.png" },
+  { id: "125", name: "Headphones", price: "123.99$", image: "product3.jpg" },
+  { id: "126", name: "Samsung phone", price: "389.99$", image: "samsung.jpeg" },
+  { id: "127", name: "Wireless headphones", price: "389.99$", image: "wireless-headphones.jpg" },
 ];
